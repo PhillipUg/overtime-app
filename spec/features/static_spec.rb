66c5549d1fdc3ffe16dc88c27ca892709b2dabc4
Phillip Musiime <phillipmusiime@gmail.com>
2021-01-03 13:13:36 +0300

    add test for homepage

---
+++ spec/features/static_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'homepage' do
+    it 'can be reached successfully' do
+      visit root_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
