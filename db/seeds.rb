c0283e2b8ba96f2342168e0659aa4105e80c693d
Phillip Musiime <phillipmusiime@gmail.com>
2021-01-04 15:46:12 +0300

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,9 +7,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@user = User.create!(email: 'test@test.com', password: 'secret', password_confirmation: 'secret', first_name: 'John', last_name: 'mcntyre')
+
+puts '1 User created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts '100 Posts have been created'
