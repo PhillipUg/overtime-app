ba7448199f6960e0448f6b4542028de332cc5c2f
Phillip Musiime <phillipmusiime@gmail.com>
2021-01-05 11:38:50 +0300

    implement custom full_name method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name
   has_many :posts
+
+  def full_name
+    "#{last_name.capitalize}, #{first_name.capitalize}"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create!(email: 'test@test.com', password: 'secret', password_confirmation: 'secret', first_name: 'John', last_name: 'mcntyre')
+  end
   describe 'creation' do
-    let(:user) { User.create!(email: 'test@test.com', password: 'secret', password_confirmation: 'secret', first_name: 'John', last_name: 'mcntyre') }
     it 'can be created' do
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it 'cannot be created without a first_name and last_name' do
-      user.first_name = nil
-      user.last_name = nil
+      @user.first_name = nil
+      @user.last_name = nil
 
-      expect(user).to_not be_valid
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe 'custom name method' do
+    it 'has a full name method that combines first and last names' do
+      expect(@user.full_name).to eq("Mcntyre, John")
     end
   end
 end
