b202d3617e4821d49d8cc1fb3e4116d2a5e313ff
phillipug <phillipmusiime@gmail.com>
2021-01-23 09:47:59 +0300

    implement status enum into posts for approval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   validates_presence_of :date, :rationale
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@user = User.create!(email: 'test@test.com', password: 'secret', password_confirmation: 'secret', first_name: 'John', last_name: 'mcntyre')
+@user = User.create!(email: 'test@test.com', password: 'secret', password_confirmation: 'secret', first_name: 'John',
+                     last_name: 'mcntyre')
 
 puts '1 User created'
+
+AdminUser.create!(email: 'cashguy@email.com', password: 'cash1234', password_confirmation: 'cash1234',
+                  first_name: 'cash', last_name: 'guy')
+
+puts '1 AdminUser created'
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
